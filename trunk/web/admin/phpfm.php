3bc2269afc97b404e6cf93ab90c02be96b799ec0
Andy-qinyuhai <54092041+Andy-qinyuhai@users.noreply.github.com>
2023-09-28 16:53:34 +0800

    Update phpfm.php

--- trunk/web/admin/phpfm.php
+++ trunk/web/admin/phpfm.php
@@ -19,6 +19,7 @@ require_once('../include/my_func.inc.php');
         $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
     }
 	// Server Vars
+	/*
 	if (isset($_GET['pid'])) {
 		$pid=intval($_GET['pid']);
 	}
@@ -30,6 +31,7 @@ require_once('../include/my_func.inc.php');
 	echo $_SESSION[$OJ_NAME.'_'.'administrator'];
 	echo "<a href='../loginpage.php'>No Privilege.</a>";
 	exit(1);
+	*/
 }
 // this is not a webshell , and it need administrator / problem editor / problem owner  membership to use, 
     function get_client_ip() {

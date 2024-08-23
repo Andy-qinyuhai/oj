8483f08d5006595002e0acfa6d4b15189289b9ad
Andy-qinyuhai <54092041+Andy-qinyuhai@users.noreply.github.com>
2024-08-23 21:43:39 +0800

    fix bug

--- trunk/web/template/syzoj/status.php
+++ trunk/web/template/syzoj/status.php
@@ -156,7 +156,7 @@
                 echo "'$result',";
         } ?>
         ''];
-   <?php echo " var oj_mark='$OJ_MARK'; "?>
+   <?php echo " var oj_mark='$OJ_MARK'; ";?>
 </script>
         <script src="template/syzoj/auto_refresh.js?v=0.52" ></script>
 

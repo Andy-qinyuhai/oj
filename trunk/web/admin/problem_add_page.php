0e629305eb01ca9b3e47eaa717b7da0d751051f3
Andy-qinyuhai <54092041+Andy-qinyuhai@users.noreply.github.com>
2024-09-13 09:50:01 +0800

    Update problem_add_page.php

--- trunk/web/admin/problem_add_page.php
+++ trunk/web/admin/problem_add_page.php
@@ -179,7 +179,7 @@
 	preview.find("#sinput").html(sinput);
 	let soutput=$("textarea").eq(7).val();
 	preview.find("#soutput").html(soutput);
-	let hint=$("textarea").eq(12).val();
+	let hint=$("textarea").eq(13).val();
 	preview.find("#hint").html(hint);
 	preview.find("#hint .md").each(function(){
 		$(this).html(marked.parse($(this).html()));

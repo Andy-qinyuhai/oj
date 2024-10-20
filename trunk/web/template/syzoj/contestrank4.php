5a55f4aac0b3a38f043352ce852415d61a1aeb64
Andy-qinyuhai <54092041+Andy-qinyuhai@users.noreply.github.com>
2024-10-20 17:55:41 +0800

    格式调整

--- trunk/web/template/syzoj/contestrank4.php
+++ trunk/web/template/syzoj/contestrank4.php
@@ -14,11 +14,11 @@
      <a class='ui small blue button' href="contestrank.php?cid=<?php echo $cid?>" ><?php echo $MSG_BACK ?></a> 
 </h1>
 </div>
-<div id="rankdiv" class="padding" onclick="$(this).css('margin-left',(548-window.innerWidth/2)+'px').css('margin-right',(548-window.innerWidth/2)+'px')" style="margin-left:-200px;margin-right:-200px;overflow:auto" >
+<div id="rankdiv" class="padding" onclick="$(this).css('margin-left',(548-window.innerWidth/2)+'px').css('margin-right',(548-window.innerWidth/2)+'px')" style="margin-left:-100px;margin-right:-100px;overflow:auto" >
 
     <?php if($user_cnt>0){ ?>
     <table    <?php if (isset($_GET['down'])) echo "border=1" ?>
-                                 class="ui very basic center aligned table" style="margin:30px">
+                                 class="ui very basic center aligned table" style="margin:20px">
         <thead>
             <tr class='active'>
                <td class="{sorter:'false'} text-center"><?php echo $MSG_STANDING?></td>
--- trunk/web/template/syzoj/contestrank5.php
+++ trunk/web/template/syzoj/contestrank5.php
@@ -13,10 +13,10 @@
 <a class='ui small blue button' href="contestrank-oi.php?cid=<?php echo $cid?>" ><?php echo $MSG_BACK ?></a> 
 </h1>
 </div>
-<div id="rankdiv" class="padding" onclick="$(this).css('margin-left',(548-window.innerWidth/2)+'px').css('margin-right',(548-window.innerWidth/2)+'px')" style="margin-left:-200px;margin-right:-200px;overflow:auto" >
+<div id="rankdiv" class="padding" onclick="$(this).css('margin-left',(548-window.innerWidth/2)+'px').css('margin-right',(548-window.innerWidth/2)+'px')" style="margin-left:-100px;margin-right:-100px;overflow:auto" >
 
 	<?php if ($user_cnt > 0) { ?>
-		<table class="ui very basic center aligned table" style="margin:30px">
+		<table class="ui very basic center aligned table" style="margin:20px">
 			<thead>
 				<tr class='active'>
 					    <td class="{sorter:'false'} text-center"><?php echo $MSG_STANDING?></td>

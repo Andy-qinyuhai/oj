595a43fdb903ca15f21b39b106c2e8b2f53d835c
Andy-qinyuhai <54092041+Andy-qinyuhai@users.noreply.github.com>
2025-06-13 10:31:42 +0800

    Update fm.php

--- trunk/web/admin/fm.php
+++ trunk/web/admin/fm.php
@@ -17,9 +17,10 @@ define('APP_TITLE', 'Tiny File Manager');
 
 // --- EDIT BELOW CONFIGURATION CAREFULLY ---
 
-if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator'])||isset($_SESSION[$OJ_NAME.'_'.'contest_creator'])||isset($_SESSION[$OJ_NAME.'_'.'problem_editor'])||isset($_SESSION[$OJ_NAME.'_'.'password_setter']))){
-	echo "<a href='../loginpage.php'>Please Login First!</a>";
-	exit(1);
+require_once ("admin-header.php");
+if (!(isset($_SESSION[$OJ_NAME.'_'.'administrator']) || isset($_SESSION[$OJ_NAME.'_'.'problem_editor']))) {
+  echo "<a href='../loginpage.php'>Please Login First!</a>";
+  exit(1);
 }
 // Auth with login/password
 // set true/false to enable/disable it
